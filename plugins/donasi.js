686639d8d6995438b06d4ca074930f49c4d1b3dc
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-07 15:38:53 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -4,8 +4,9 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
 ┌〔 Donasi • Emoney 〕
 ├ GoPay: 085759662798 [Larass]
-├ Dana: 087832147584 [Pangera]
+├ Dana: 087832147584 [Pangeran]
 ├ Pulsa: 085759662798 [Larass]
+├ Pulsa: 087832147584 [Pangeran]
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
